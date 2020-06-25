a7c92b3670402cc32a076c04fdfecf67fb2ec472
cgmbauer <cgmbauer@gmail.com>
2020-06-24 22:27:59 -0300

    add methods: all, getBalance and create

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface Create {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface All {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,16 +24,46 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): All {
+    const balance = this.getBalance();
+
+    const transactions = this.transactions;
+    
+    const allTransactionsAndBalance = {
+      transactions,
+      balance,
+    }
+
+    return allTransactionsAndBalance;
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    const income = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce((total, value) => value.value + total, 0);
+
+      const outcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((total, value) => value.value + total, 0);
+
+      const total = income - outcome;
+
+      const balance: Balance = {
+        income,
+        outcome,
+        total,
+      }
+      
+      return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({title,type, value}: Create): Transaction {
+    const transaction = new Transaction({title, value, type});
+
+    this.transactions.push(transaction)
+
+    return transaction;
   }
 }
 
